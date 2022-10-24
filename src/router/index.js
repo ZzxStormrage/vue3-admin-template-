3bef8c7a6b0a6d24e7e1ad949f175aed1c467617
zzx <452436275@qq.com>
2022-10-24 18:15:10 +0800

    ok

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@
  * @Author: zzx 452436275@qq.com
  * @Date: 2022-10-21 16:27:37
  * @LastEditors: zzx 452436275@qq.com
- * @LastEditTime: 2022-10-21 17:23:56
+ * @LastEditTime: 2022-10-24 17:58:42
  * @FilePath: /vue3-app/src/router/index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHea
  * der查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
@@ -18,20 +18,20 @@ export const fixedRoutes = [...home]
 export const asyncRoutes = []
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {
-            path: '/',
-            redirect: '/home',
-        },
-        ...fixedRoutes,
-    ],
-    scrollBehavior(to, from, savedPosition) {
-        if (savedPosition) {
-            return savedPosition
-        } 
-        return { left: 0, top: 0 }
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      redirect: '/home',
     },
+    ...fixedRoutes,
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } 
+    return { left: 0, top: 0 }
+  },
 })
 
 export default router
---
+++ src/views/home/layout/index.vue
@@ -0,0 +1,15 @@
+<!--
+ * @Author: zzx 452436275@qq.com
+ * @Date: 2022-10-24 18:02:05
+ * @LastEditors: zzx 452436275@qq.com
+ * @LastEditTime: 2022-10-24 18:07:19
+ * @FilePath: /vue3-app/src/views/home/layout/index.vue
+-->
+<script setup>
+
+</script>
+<template>
+    layout
+</template>
+<style>
+</style>
