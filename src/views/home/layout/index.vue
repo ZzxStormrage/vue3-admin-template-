f19e2d847b13f8d20d0e8e700e676c561d446f7b
zzx <452436275@qq.com>
2022-10-27 10:48:54 +0800

    ok

--- src/views/home/layout/index.vue
+++ src/views/home/layout/index.vue
@@ -2,7 +2,7 @@
  * @Author: zzx 452436275@qq.com
  * @Date: 2022-10-24 18:02:05
  * @LastEditors: zzx 452436275@qq.com
- * @LastEditTime: 2022-10-24 18:07:19
+ * @LastEditTime: 2022-10-27 10:39:48
  * @FilePath: /vue3-app/src/views/home/layout/index.vue
 -->
 <script setup>
