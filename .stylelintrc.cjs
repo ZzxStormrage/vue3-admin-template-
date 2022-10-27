5bb4d49d8b87489b814d4858e0ee6a408e3610c8
zzx <452436275@qq.com>
2022-10-27 10:36:21 +0800

    ok

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,41 +1,37 @@
 /*
- * @Author: zzx 452436275@qq.com
  * @Date: 2022-10-24 15:48:05
  * @LastEditors: zzx 452436275@qq.com
- * @LastEditTime: 2022-10-24 16:11:48
+ * @LastEditTime: 2022-10-27 10:30:21
  * @FilePath: /vue3-app/.stylelintrc.cjs
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 module.exports = {
-  extends: [
-    'stylelint-config-standard-scss',
-    'stylelint-config-recommended-vue'
-  ],
-  customSyntax: 'postcss-html',
   overrides: [
     {
-      files: ['**/*.{scss,css,sass}'], // css 相关文件由 postcss-scss 处理
-      customSyntax: 'postcss-scss'
-    }
+      files: ['*.vue', '**/*.vue', '*.html', '**/*.html'],
+      extends: ['stylelint-config-html'],
+      rules: {
+        // 指定关键帧名称的模式
+        'keyframes-name-pattern': null,
+        // 禁止未知的伪类选择器
+        'selector-pseudo-class-no-unknown': [
+          true,
+          {
+            ignorePseudoClasses: ['deep', 'global'],
+          },
+        ],
+        // 禁止未知的伪元素选择器
+        'selector-pseudo-element-no-unknown': [
+          true,
+          {
+            ignorePseudoElements: ['v-deep', 'v-global', 'v-slotted'],
+          },
+        ],
+      },
+    },
+    {
+      files: ['*.less', '**/*.less'],
+      customSyntax: 'postcss-less',
+      extends: ['stylelint-config-standard', 'stylelint-config-recommended-vue'],
+    },
   ],
-  rules: {
-    indentation: 2,
-    'selector-pseudo-element-no-unknown': [
-      true,
-      {
-        ignorePseudoElements: ['v-deep']
-      }
-    ],
-    'number-leading-zero': 'never',
-    'no-descending-specificity': null,
-    'font-family-no-missing-generic-family-keyword': null,
-    'selector-type-no-unknown': null,
-    'at-rule-no-unknown': null,
-    'no-duplicate-selectors': null,
-    'no-empty-source': null,
-    'selector-pseudo-class-no-unknown': [
-      true,
-      { ignorePseudoClasses: ['global'] }
-    ]
-  }
-}
+};
